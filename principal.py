d8e6df95488a371ffe7e196cb903de4492455597
Iuryalves0 <iuryalves2317@gmail.com>
2024-10-24 17:28:41 +0000

    add funÇão inss

---
+++ principal.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls || clear")
+
+#inicicalmente no terminal coloca-se, "git branch" para saber quantas branchs e qual a principal
+#para passar para outras brenchs, alternar entre elas "git checkout" -b  (caso nao haja, outra sera criada)
+#git checkout [nome da função] "isso é pra ir pra o git certo"
+def inss():
+    #considere o feito.
+    pass
