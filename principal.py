36fa87dc7ed5b3c5df7ff5954bd74aae40884a55
Iuryalves0 <iuryalves2317@gmail.com>
2024-10-24 17:36:40 +0000

    titulo

--- principal.py
+++ principal.py
@@ -4,6 +4,7 @@ os.system("cls || clear")
 #inicicalmente no terminal coloca-se, "git branch" para saber quantas branchs e qual a principal
 #para passar para outras brenchs, alternar entre elas "git checkout" -b  (caso nao haja, outra sera criada)
 #git checkout [nome da função] "isso é pra ir pra o git certo"
+
 def inss():
     #considere o feito.
     pass
